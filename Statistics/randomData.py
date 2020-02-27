62f0b29cf7890db3940b3d5e09d595bb55bfbcdf
Noura.Hedhli <nh98@njit.edu>
2020-02-27 15:55:30 -0500

    Task: Random without Seed

--- Statistics/randomData.py
+++ Statistics/randomData.py
@@ -1,6 +1,8 @@
 import random
 import pprint
 
+import decimal
+
 
 def random_code():
     random.seed(3)
@@ -8,6 +10,21 @@ def random_code():
     i = 1
     while i < 6:
         randomData.append(random.randint(1, 100))
+        x = "{0:.2f}".format(random.random())
+        randomData.append(x)
         i += 1
-        pprint.pprint(randomData)
+    pprint.pprint(randomData)
     return randomData
+
+
+def random_code_withoutSeed():
+    randomData1 = []
+    i = 1
+    while i < 3:
+        randomData1.append(random.randint(4, 300))
+        x = "{0:.2f}".format(random.random())
+        randomData1.append(x)
+        i += 1
+
+    pprint.pprint(randomData1)
+    return randomData1
--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -4,20 +4,21 @@ from Statistics.Statistics import Statistics
 import pprint
 import numpy as np
 from Statistics.randomData import random_code
+from Statistics.randomData import random_code_withoutSeed
 
 
 class MyTestCase(unittest.TestCase):
     def setUp(self) -> None:
-
         self.testData = random_code()
         self.statistics = Statistics()
+        self.testData1 = random_code_withoutSeed()
 
     def test_instantiate_calculator(self):
         self.assertIsInstance(self.statistics, Statistics)
 
     def test_mean_calculator(self):
         mean = self.statistics.mean(self.testData)
-        self.assertEqual(mean, 48.4)
+        self.assertEqual(mean, 48.0)
 
 
 if __name__ == '__main__':
