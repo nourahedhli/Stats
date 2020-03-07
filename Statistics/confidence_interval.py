20d20cdb637b98f0a627496e1d420aad553afc1f
Noura.Hedhli <nh98@njit.edu>
2020-03-07 00:59:27 -0500

    Add: confidence_interval

--- Statistics/confidence_interval.py
+++ Statistics/confidence_interval.py
@@ -1,9 +1,8 @@
-import numpy as np
-from Calculator.squareRoot import *
+
 from statistics import mean
 from Statistics.standardDeviation import StandardDeviationSample
-from Statistics.Zscores import zscore
-from scipy import stats
+from Statistics.Zscores import Z_scores
+
 
 from Statistics.Zscores import z_values
 
@@ -13,7 +12,7 @@ def mean_confidence_interval(data):
     List = []
     std = StandardDeviationSample(data)
     mu = mean(data)
-    z_list = zscore(z_values(data))
+    z_list = Z_scores(z_values(data))
     SE = std / n
     for i in z_list:
         left = mu - i * SE
