780089dc0a845e7ea98a4f2ae2334ddd704d87da
Noura.Hedhli <nh98@njit.edu>
2020-03-13 20:29:51 -0400

    Fix SampleMean

--- Statistics/SampleMean.py
+++ Statistics/SampleMean.py
@@ -5,11 +5,7 @@ from Statistics.Getsample import getSample
 
 def sample_mean(data, sample_size):
     total = 0
-    # check that get sample returns the proper number of samples
-    # check that sample size is not 0
-    # check that sample size is not larger than the population
-    # https://realpython.com/python-exceptions/
-    # https://stackoverflow.com/questions/129507/how-do-you-test-that-a-python-function-throws-an-exception
+
     sample = getSample(data, sample_size)
     num_values = len(sample)
     for num in sample:
